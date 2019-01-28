cd46fcbc2257694b66e6ac7c3d33d5db9395d646
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-01-28 15:22:50 -0600

    Using setTimeout(), I made the card translate back itself.

--- sliderFunctionable.js
+++ sliderFunctionable.js
@@ -3,12 +3,19 @@ function slidingBox() {
     var motion = document.querySelector(".card");
     expand.addEventListener("click", function() {
         
-        if(motion.classList.contains("slider")) {
-            motion.classList.remove("slider");
+        if(! motion.classList.contains("sliderOut")) {
+            motion.classList.add("sliderOut")
+            
         }
         else {
-            motion.classList.add("slider")
+            motion.classList.remove("sliderOut")  
         }
+
+        setTimeout(function(){motion.classList.add("sliderOut")}, 3100)   
+    
     })
+
+    
+    
 }
 slidingBox();
