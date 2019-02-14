391bbbd8faf77e3080e1810af92d2b7b978b5775
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-02-14 15:48:20 -0600

    Fixed the inputs and the button

--- sliderFunctionable.js
+++ sliderFunctionable.js
@@ -1,15 +1,8 @@
 function slidingBox() {
-  var expand = document.getElementById("expandingSliderBtn");
+  var expand = document.getElementById("expandBtn");
   var motion = document.querySelector(".card");
 
-  // missingValues();
-  // var inputs = document.getElementById("insertName").querySelectorAll('.inputs')
-  // for (const input of inputs){
-  //     input.addEventListener("onInput", () => {
-  //         missingValues();
-  //     })
-  // }
-  missingValues();
+  disablingBtn();
   expand.addEventListener("click", function() {
     if (!motion.classList.contains("sliderOut")) {
       motion.classList.add("sliderOut");
@@ -19,14 +12,14 @@ function slidingBox() {
 
     setTimeout(function() {
       motion.classList.add("sliderOut");
-    }, 3100);
+    }, 3200);
   });
 }
 
 function showPlayerName() {
   var inpt = document.querySelector("#nameBar");
   var nameArea = document.querySelector(".playerNameArea");
-  var expand = document.getElementById("expandingSliderBtn");
+  var expand = document.getElementById("expandBtn");
 
   expand.addEventListener("click", function() {
     nameArea.innerHTML = inpt.value;
@@ -36,7 +29,7 @@ function showPlayerName() {
 function showPlayerTd() {
   var inpt = document.querySelector("#tdBar");
   var nameArea = document.querySelector(".stats-td");
-  var expand = document.getElementById("expandingSliderBtn");
+  var expand = document.getElementById("expandBtn");
 
   expand.addEventListener("click", function() {
     nameArea.innerHTML = inpt.value + " Touchdowns";
@@ -46,7 +39,7 @@ function showPlayerTd() {
 function showPlayerInt() {
   var inpt = document.querySelector("#intBar");
   var nameArea = document.querySelector(".stats-int");
-  var expand = document.getElementById("expandingSliderBtn");
+  var expand = document.getElementById("expandBtn");
 
   expand.addEventListener("click", function() {
     nameArea.innerHTML = inpt.value + " Interceptions";
@@ -56,7 +49,7 @@ function showPlayerInt() {
 function showPlayerYds() {
   var inpt = document.querySelector("#yrdBar");
   var nameArea = document.querySelector(".stats-yrd");
-  var expand = document.getElementById("expandingSliderBtn");
+  var expand = document.getElementById("expandBtn");
 
   expand.addEventListener("click", function() {
     nameArea.innerHTML = inpt.value + " Yards";
@@ -66,7 +59,7 @@ function showPlayerYds() {
 function showPlayerRate() {
   var inpt = document.querySelector("#rateBar");
   var nameArea = document.querySelector(".stats-rate");
-  var expand = document.getElementById("expandingSliderBtn");
+  var expand = document.getElementById("expandBtn");
 
   expand.addEventListener("click", function() {
     nameArea.innerHTML = inpt.value + " Rating";
@@ -76,31 +69,110 @@ function showPlayerRate() {
 function showPlayerImg() {
   var inpt = document.querySelector("#imgBar");
   var nameArea = document.querySelector(".img-one");
-  var expand = document.getElementById("expandingSliderBtn");
+  var expand = document.getElementById("expandBtn");
 
   expand.addEventListener("click", function() {
     nameArea.src = inpt.value;
   });
 }
 
-function missingValues() {
-  var rateInpt = document.querySelector("#rateBar");
-  var intInpt = document.querySelector("#intBar");
-  var yrdInpt = document.querySelector("#yrdBar");
-  var tdInpt = document.querySelector("#tdBar");
-  var inpt = document.querySelector("#nameBar");
-  var expand = document.getElementById("expandingSliderBtn");
-
-  console.log(rateInpt.value);
-
-  if (rateInpt.value == "") {
-    expand.style.display = "none";
-  } else if (rateInpt.value == " ") {
-    expand.style.display = "block";
-  } else {
-    expand.style.display = "block";
-  }
+function disablingBtn() {
+  inputsNameDisabling()
+  inputsImgDisabling()
+  inputsIntDisabling()
+  inputsRateDisabling()
+  inputsTdDisabling()
 }
+
+function inputsImgDisabling() {
+  expandBtn = document.getElementById("expandBtn");
+  expandBtn.disabled = true;
+  playerInput = document.getElementById("imgBar");
+  playerInput.addEventListener("keyup", function() {
+    if (playerInput.value == "") {
+      expandBtn.classList.remove("sliding-btn:hover ")
+
+      expandBtn.disabled = true;
+    } else {
+      expandBtn.disabled = false;
+    }
+  });
+}
+
+function inputsRateDisabling() {
+  expandBtn = document.getElementById("expandBtn");
+  expandBtn.disabled = true;
+  playerInput = document.getElementById("rateBar");
+  playerInput.addEventListener("keyup", function() {
+    if (playerInput.value == "") {
+      expandBtn.classList.remove("sliding-btn:hover ")
+
+      expandBtn.disabled = true;
+    } else {
+      expandBtn.disabled = false;
+    }
+  });
+}
+
+function inputsYrdDisabling() {
+  expandBtn = document.getElementById("expandBtn");
+  expandBtn.disabled = true;
+  playerInput = document.getElementById("yrdBar");
+  playerInput.addEventListener("keyup", function() {
+    if (playerInput.value == "") {
+      expandBtn.classList.remove("sliding-btn:hover ")
+      expandBtn.disabled = true;
+    } else {
+      expandBtn.disabled = false;
+    }
+  });
+}
+
+function inputsTdDisabling() {
+  expandBtn = document.getElementById("expandBtn");
+  expandBtn.disabled = true;
+  playerInput = document.getElementById("tdBar");
+  playerInput.addEventListener("keyup", function() {
+    if (playerInput.value == "") {
+      expandBtn.disabled = true;
+    } else {
+      expandBtn.disabled = false;
+    }
+  });
+}
+
+function inputsNameDisabling() {
+  expandBtn = document.getElementById("expandBtn");
+  expandBtn.disabled = true;
+  playerInput = document.getElementById("nameBar");
+  playerInput.addEventListener("keyup", function() {
+    
+    if (playerInput.value == "") {
+      expandBtn.classList.remove("sliding-btn:hover ")
+
+      expandBtn.disabled = true;
+    } else {
+      expandBtn.disabled = false;
+    }
+  });
+}
+
+function inputsIntDisabling() {
+  expandBtn = document.getElementById("expandBtn");
+  expandBtn.disabled = true;
+  playerInput = document.getElementById("intBar");
+  playerInput.addEventListener("keyup", function() {
+    if (playerInput.value == "") {
+      expandBtn.classList.remove("sliding-btn:hover ")
+
+      expandBtn.disabled = true;
+    } else {
+      expandBtn.disabled = false;
+    }
+  });
+}
+
+
 
 slidingBox();
 showPlayerName();
